adae40489a3cca978b1f16c2c6290412b8366bbd
Nilqua <savezarzath@gmail.com>
2023-07-18 10:12:37 +0700

    Q2

--- Q2.c
+++ Q2.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 void main () {
-    int x,y,sum=0;
-    scanf ("%d",&x);
-    scanf ("%d",&y);
+    int x,y;
+    int sumx=0,sumy=0,sumxy=0;
+    printf ("Enter fisrt and second number (fist second) : ");
+    scanf ("%d %d",&x,&y);
+    while (1){
+        int num;
+        printf ("Enter number : ");
+        scanf ("%d",&num);
+        if (num <=0) {
+            break;
+        }
+        if (num%x==0 && num%y!=0){
+            sumx += num;
+        }
+        if (num%x!=0 && num%y==0){
+            sumy += num;
+        }
+        if (num%x==0 || num%y==0){
+            sumxy += num;
+        }
+    }
+    printf ("%d\n%d\n%d",sumx,sumy,sumxy);
 }
