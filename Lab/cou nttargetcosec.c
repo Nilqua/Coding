48a7b519c7e0e0226bdc2e17c7d1d6f0efa714a5
Nilqua <savezarzath@gmail.com>
2023-08-26 12:58:53 +0700

    Change folder and files

---
+++ Lab/cou nttargetcosec.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+void main ()
+{
+    int u_input,u_input_old;
+    int intrest;
+    int same,count;
+    scanf ("%d",&intrest);
+    for (same=0,count=0,u_input_old=intrest,u_input=1; u_input!=0;)
+    {
+        scanf ("%d",&u_input);
+        if (u_input == intrest)
+        {
+            same += 1;
+        }
+        if (u_input == u_input_old)
+        {
+            count += 1;
+        }
+        u_input_old=u_input;
+    }
+    printf ("%d\n%d",count,same);
+}
